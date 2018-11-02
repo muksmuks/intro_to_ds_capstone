63433a53d949a4abe98f05a5173159f2f34ce98f
mukeshkumar <mkumar4n@in.ibm.com>
2018-11-02 13:13:51 +0530

    capstone.py

--- capstone.py
+++ capstone.py
@@ -1,5 +1,7 @@
 #This script will be used for capstone project - Applied Data Science
+
+#import required libraries
 import pandas as pd
 import numpy as np
 
-print('Hello Capstone Project Course!')
+print('Hello Capstone Project Course')
